56bc5344cc52ad1e0253f77196479bceda6b119b
Mark Scherer <mark@Marks-MacBook-Pro.local>
2021-12-01 20:47:32 -0600

    initial commit: setup react app

---
+++ display-frontend/src/Components/Display.js
@@ -0,0 +1,13 @@
+/*
+  main 'frame' to host all slides
+*/
+
+import React from 'react';
+
+class Display extends React.Component {
+  render() {
+    return <h2>Hi, I am Display!</h2>
+  }
+}
+
+export default Display
