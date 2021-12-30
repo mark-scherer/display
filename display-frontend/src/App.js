a4786e5e7cebb26d9bcd29cf3b1fadd64749842e
Mark Scherer <mark@Marks-MacBook-Pro.local>
2021-12-29 21:54:27 -0500

    attempted to add google analytics

--- display-frontend/src/App.js
+++ display-frontend/src/App.js
@@ -1,6 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 import Display from './Components/Display.js'
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-W3NDJ522RY')
+ReactGA.pageview(window.location.pathname + window.location.search)
 
 function App() {
   return (
